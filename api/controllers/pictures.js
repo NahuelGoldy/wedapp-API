ba539492bdc67aa3752bb654c892f2f3e22526fc
Nahuel SG <nahuelsg.64@gmail.com>
2018-07-13 16:07:05 -0300

    Firebase separate storage for image files

--- api/controllers/pictures.js
+++ api/controllers/pictures.js
@@ -1,24 +1,57 @@
 
 const mongoose = require('mongoose');
 const Pic = new require('../models/picture');
+const request = require('request');
+const axios = require('axios');
 
 // TODO review "batch" size
 const batchSize = 12;
 
 // create and save a pic
 exports.post = (req, res) => {
-    const data = req.body ;
+    const data = req.body;
+    const img = req.file;
+    const img_name = (new Date().toISOString().split('.')[0]).replace(/:/g, '.') + '.' + req.file.originalname.split('.').pop();
 
-    return Pic.create({
-        _id: mongoose.Types.ObjectId(),
-        path: req.file.path,
-        isPublic: data.isPublic
-    })
-        .then(post => {
-            return post;
+    const firebase_url = 'https://firebasestorage.googleapis.com/v0/b/wedapp-1529785854543.appspot.com/o?uploadType=media&name=' + img_name;
+    let fb_base_download_url = 'https://firebasestorage.googleapis.com/v0/b/';
+    const formData = {
+        image: {
+            value: img.buffer,
+            options: {
+                filename: img_name
+            }
+        }
+    };
+    const options = {
+        uri: firebase_url,
+        formData: formData,
+        method: 'POST'
+    };
+
+    // let fb_response = {};
+    // Send POST to Firebase storage to save just the image
+    // request(options, (err, response, body) => {
+    //     if (err) console.log('Request err: ', err); // TODO improve error handling (retry strategy?)
+    //     fb_response = JSON.parse(body);
+    // });
+
+    return axios.post(options.uri, options.formData)
+        .then(response => {
+            // console.log(response.data);
+            const fb_response = response.data;
+
+            Pic.create({
+                _id: mongoose.Types.ObjectId(),
+                path: fb_base_download_url + response.data.bucket + '/o/' + response.data.name + '?alt=media&token=' + response.data.downloadTokens,
+                isPublic: data.isPublic
+            })
+                .then(pic => {
+                    return pic;
+                })
         })
         .catch(err => {
-            res.status(500).send(err);
+            return err;
         });
 };
 
@@ -31,7 +64,7 @@ exports.findAll = (req, res) => {
         .catch(err => {
             res.status(500).send(err);
         });
-}
+};
 
 // return all Pics
 exports.findAllPagination = (req, res) => {
