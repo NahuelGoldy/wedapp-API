95c9f39cd4a9c4ee786296503dfe87156ee8da6d
Nahuel SG <nahuelsg.64@gmail.com>
2018-07-15 00:38:45 -0300

    Config refactor

--- api/config/config.js
+++ api/config/config.js
@@ -5,11 +5,13 @@ const config = {};
 // config.hostname = 'dev.example.com';
 
 //firebase storage
-config.firebase = {}
-// (base URL for upload)
-config.firebase.url_upload = 'https://firebasestorage.googleapis.com/v0/b/wedapp-1529785854543.appspot.com/o?uploadType=media&name=';
-// (base URL for download)
-config.firebase.url_download = 'https://firebasestorage.googleapis.com/v0/b/';
+config.firebase = {};
+// Base URL
+config.firebase.url = 'https://firebasestorage.googleapis.com/v0/b/';
+// Firebase bucket
+config.firebase.bucket = 'wedapp-1529785854543.appspot.com/';
+// upload query params
+config.firebase.upload_params = 'o?uploadType=media&name=';
 
 //mongo database
 config.mongo = {};
